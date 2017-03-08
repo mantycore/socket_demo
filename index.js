05a55fa834bd43de192b52efedbed6d1c9137c33
Dmitry Kourmyshov <mantycore@gmail.com>
2017-03-08 23:13:13 +0300

    Added connection to Puredata

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var net = require('net');
 var express = require('express')
 var app = express();
 var server = require('http').createServer(app);
@@ -7,18 +8,46 @@ app.use(express.static('public'));
 
 var pool = [];
 function getFree() {
-	var i = pool.indexOf(null);
-	return i !== -1 ? i : pool.length;
+    var i = pool.indexOf(null);
+    return i !== -1 ? i : pool.length;
 }
 
 io.on('connection', function (client) {
   var index = getFree();
   pool[index] = client;
   console.log("connect", index);
-  client.on('data', function (data) { console.log("data", index, data); });
+  client.on('data', function (data) {
+    console.log("data", index, data);
+    if (connection_established) {
+        //note: client index is unused, any client sends same two parameters
+        puredata.write("x " + data.x + ";");
+        puredata.write("y " + data.y + ";");
+        console.log("Sent to pd");
+    }
+  });
   client.on('disconnect', function () {
-  	console.log("disconnect", index);
+    console.log("disconnect", index);
     pool[index] = null;
   });
 });
+
+var puredata = new net.Socket();
+var connection_established = false;
+puredata.connect(3001, '127.0.0.1', function() {
+    console.log('Connected to pd');
+    puredata.write('hello;');
+    connection_established = true;
+});
+
+puredata.on('data', function(data) {
+    console.log('Received: ' + data);
+    //client.destroy(); // kill client after server's response
+});
+
+puredata.on('close', function() {
+    console.log('Connection closed');
+    connection_established = false;
+    //in real application, we should probably try to reconnect after this
+});
+
 server.listen(3000);
